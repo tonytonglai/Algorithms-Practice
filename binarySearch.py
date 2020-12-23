6f9060fe9ccdd23e0c3a7b0295374d7c00a94489
Tony Lai <tonytonglai2000@gmail.com>
2020-12-23 16:21:10 -0500

    practicing github with pycharm
    also practicing python and algos <3

---
+++ binarySearch.py
@@ -0,0 +1,25 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high)//2
+        guess = list[mid]
+
+        #three cases
+        #if guess is just right
+        if guess == item:
+            return mid
+
+        #if guess is less than item
+        if guess > item:
+            high = mid - 1
+
+        #if guess is greater than item
+        else:
+            low = mid + 1
+
+    return None
+
+my_list = [1 , 3 , 5 , 7, 9]
+print(binary_search(my_list, 7))
